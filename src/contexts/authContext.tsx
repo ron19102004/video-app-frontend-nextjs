02d463e24b4f4c91676ef11e2e58ff168b93d65c
ron19102004 <ron19102004@gmail.com>
2024-06-16 01:58:41 +0700

    fix

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -7,11 +7,16 @@ export const AuthContext = createContext<IUseAuth>({
   isAuth: false,
   userCurrent: null,
   vip: null,
-  register: function (registerRequest: RegisterRequest, redirect: (href: string) => void): Promise<void> {
+  register: function (
+    registerRequest: RegisterRequest,
+    redirect: (href: string) => void
+  ): Promise<void> {
     throw new Error("Function not implemented.");
   },
-  login: function (loginRequest: LoginRequest, redirect: (
-    href: string) => void): Promise<void> {
+  login: function (
+    loginRequest: LoginRequest,
+    redirect: (href: string) => void
+  ): Promise<void> {
     throw new Error("Function not implemented.");
   },
   logout: function (redirect: (href: string) => void): void {
@@ -19,7 +24,14 @@ export const AuthContext = createContext<IUseAuth>({
   },
   checkAuthentication: function (): void {
     throw new Error("Function not implemented.");
-  }
+  },
+  verifyOTP: function (
+    data: { otp: string; token: string },
+    redirect: (href: string) => void,
+    toast: (message: string) => void
+  ): Promise<void> {
+    throw new Error("Function not implemented.");
+  },
 });
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return (
